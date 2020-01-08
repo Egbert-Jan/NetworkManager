1c6cc1a9869b9db825e1dc878e0733151e94af2e
Egbert-Jan <ejterpstra@icloud.com>
2020-01-08 10:35:52 +0100

    Can send plain requests and tests

--- Sources/NetworkManager/NetworkManager.swift
+++ Sources/NetworkManager/NetworkManager.swift
@@ -1,3 +1,67 @@
-struct NetworkManager {
-    var text = "Hello, World!"
+import Foundation
+
+class NetworkManager {
+    
+    func request(target: NMTarget, completion: @escaping ((Result<NMResponse, Error>) -> Void)) {
+
+        var request = URLRequest(url:
+            target.baseUrl.appendingPathComponent(target.path))
+        request.httpMethod = target.method.rawValue
+        
+        if target.method != .get {
+            let body: [String: Any] = ["hoi": "Hello", "age": 5]
+            request.httpBody = try! JSONSerialization.data(withJSONObject: body, options: [])
+        }
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            
+            if let data = data, let response = response {
+                completion(.success(NMResponse(data: data, response: response)))
+            } else {
+                completion(.failure(NMError.emptyResponseOrData))
+            }
+        }
+        
+        dataTask.resume()
+    }
+}
+
+protocol NMTarget {
+    var baseUrl: URL { get }
+    var path: String { get }
+    var method: NMMethod { get }
+}
+
+enum NMMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
+enum NMTask {
+    case plainRequest
+    case urlParameters([String: Any])
+    case bodyParameters([String: Any])
+//    case bodyAndUrlParameters
+}
+
+struct NMResponse {
+    var data: Data
+    var response: URLResponse
+    
+    func getDecodedObject<T: Decodable>(object: T.Type) -> T? {
+        do {
+            return try JSONDecoder().decode(object, from: data)
+        } catch {
+            return nil
+        }
+    }
+}
+
+enum NMError: Error {
+    case emptyResponseOrData
 }
--- Tests/NetworkManagerTests/NetworkManagerTests.swift
+++ Tests/NetworkManagerTests/NetworkManagerTests.swift
@@ -2,14 +2,103 @@ import XCTest
 @testable import NetworkManager
 
 final class NetworkManagerTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(NetworkManager().text, "Hello, World!")
+    
+    var networkManager = NetworkManager()
+    
+    func testRawGet() {
+        let exp = expectation(description: "succesfull response")
+        
+        networkManager.request(target: PostmanEcho.postmanGETTest) { result in
+            switch result {
+            case .success(let response):
+                XCTAssert(response.data.count > 0)
+                exp.fulfill()
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+        }
+        waitForExpectations(timeout: 1, handler: nil)
     }
-
+    
+    func testRawPost() {
+        let exp = expectation(description: "succesfull response")
+        
+        networkManager.request(target: PostmanEcho.postmanPOSTTest) { result in
+            switch result {
+            case .success(let response):
+                XCTAssert(response.data.count > 0)
+                exp.fulfill()
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+        }
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func testRawPut() {
+        let exp = expectation(description: "succesfull response")
+        
+        networkManager.request(target: PostmanEcho.postmanPUTTest) { result in
+            switch result {
+            case .success(let response):
+                XCTAssert(response.data.count > 0)
+                exp.fulfill()
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+        }
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func testRawDelete() {
+        let exp = expectation(description: "succesfull response")
+        
+        networkManager.request(target: PostmanEcho.postmanDELETETest) { result in
+            switch result {
+            case .success(let response):
+                XCTAssert(response.data.count > 0)
+                exp.fulfill()
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+        }
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("testRawGet", testRawGet),
+        ("testRawPost", testRawPost),
+        ("testRawPut", testRawPut),
+        ("testRawDelete", testRawDelete)
     ]
+}
+
+
+enum PostmanEcho: NMTarget {
+    case postmanGETTest
+    case postmanPOSTTest
+    case postmanPUTTest
+    case postmanDELETETest
+    
+    var baseUrl: URL {
+        return URL(string: "https://postman-echo.com/")!
+    }
+    
+    var path: String {
+        switch self {
+        case .postmanGETTest: return "get"
+        case .postmanPOSTTest: return "post"
+        case .postmanPUTTest: return "put"
+        case .postmanDELETETest: return "delete"
+        }
+    }
+    
+    var method: NMMethod {
+        switch self {
+        case .postmanGETTest: return .get
+        case .postmanPOSTTest: return .post
+        case .postmanPUTTest: return .put
+        case .postmanDELETETest: return .delete
+        }
+    }
 }
